a6c4f48aa299fc7613d733d66d7959bbad9e3108
Joon <evenn33111@gmail.com>
2024-08-21 17:14:37 +0900

    update

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+# This script automates setting up zsh and related utility tools
+# ./setup_zsh.sh
+
 sudo apt install -y zsh
 echo "DONE installing zsh!"
 
+[[ -e ~/.oh-my-zsh ]] | rm -r ~/.oh-my-zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "DONE installing oh-my-zsh!"
@@ -11,6 +16,8 @@ echo "DONE changing to zsh!"
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 echo "DONE downloading p10k!"
 
+SERVER_NAME="please fill"
+SERVER_INFO="please fill"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 echo "function prompt_device_info() { p10k segment -t 'server_info: $SERVER_NAME ($SERVER_INFO)]' -b 4 -f 'black'}" >> ~/.zshrc \
 && echo "POWERLEVEL9K_MODE='nerdfont-complete'" >> ~/.zshrc \
